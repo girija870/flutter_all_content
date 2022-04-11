382520b3f86352289a8d2585cb243ee47915e2d9
girija gurung <girocop20@gmail.com>
2022-04-11 18:23:53 +0545

    work_on_information_parser

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routeInformationParser: RoutesInformationParser(),
+      routeInformationParser: RoutesInformationParser(), routerDelegate: RouterDelegate,
     );
   }
 }
