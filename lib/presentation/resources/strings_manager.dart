1264bd89da0f51e4cdffaa89b4341777efcdae7a
girija gurung <girocop20@gmail.com>
2022-04-13 19:41:46 +0545

    onBoarding_view_with_viewmodel_done

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,18 @@
 class AppStrings {
   static const String noRouteFound = "No Route Found";
+  static const String onBoardingTitle1 = "SEE THE BEST COURSE #1";
+  static const String onBoardingTitle2 = "SEE THE BEST COURSE #2";
+  static const String onBoardingTitle3 = "SEE THE BEST COURSE #3";
+  static const String onBoardingTitle4 = "SEE THE BEST COURSE";
+
+  static const String onBoardingSubTitle1 =
+      "Even bad code can function. But if code isn't clean, it can bring a development organization to its knees. #1";
+  static const String onBoardingSubTitle2 =
+      "Even bad code can function. But if code isn't clean, it can bring a development organization to its knees. #2";
+  static const String onBoardingSubTitle3 =
+      "Even bad code can function. But if code isn't clean, it can bring a development organization to its knees. #3";
+  static const String onBoardingSubTitle4 =
+      "Even bad code can function. But if code isn't clean, it can bring a development organization to its knees. #4";
+
+  static const String skip = "Skip";
 }
