6bba3bb7b499459f8b997aa127c88b773f654448
girija gurung <girocop20@gmail.com>
2022-04-15 17:45:03 +0545

    _error_handler_done

---
+++ lib/data/network/failure.dart
@@ -0,0 +1,7 @@
+class Failure {
+  int code; //200 / 400
+  String message;
+
+  Failure(this.code, this.message); // success / failure
+
+}
---
+++ lib/data/repository/repository_impl.dart
@@ -0,0 +1,44 @@
+import 'package:dartz/dartz.dart';
+import 'package:flut_all_content/data/data_source/remote_data_source.dart';
+import 'package:flut_all_content/data/mapper/mapper.dart';
+import 'package:flut_all_content/data/network/failure.dart';
+import 'package:flut_all_content/data/network/network_info.dart';
+import 'package:flut_all_content/data/request/request.dart';
+import 'package:flut_all_content/domain/model.dart';
+import 'package:flut_all_content/domain/repository.dart';
+
+import '../network/error_handler.dart';
+
+class RepositoryImpl implements Repository {
+  final RemoteDataSource _remoteDataSource;
+  final NetworkInfo _networkInfo;
+
+  RepositoryImpl(this._remoteDataSource, this._networkInfo);
+
+  @override
+  Future<Either<Failure, Authentication>> login(
+      LoginRequest loginRequest) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        //its safe to call api
+        final response = await _remoteDataSource.login(loginRequest);
+        if (response.status == ApiInternalStatus.SUCCESS) // success
+        {
+          //return data success(200)
+          //return right
+          return Right(response.toDomain());
+        } else {
+          //return biz logic error
+          //return left
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        return (Left(ErrorHandler.handle(error).failure));
+      }
+    } else {
+      //return connection error
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
+    }
+  }
+}
