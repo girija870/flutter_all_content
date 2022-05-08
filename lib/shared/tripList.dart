602e3d046416d98f3e5749a3daac69c3fe0608fc
girija gurung <girocop20@gmail.com>
2022-05-08 10:51:29 +0545

    basic implicit and explicit animation

--- lib/shared/tripList.dart
+++ lib/shared/tripList.dart
@@ -10,12 +10,14 @@ class TripList extends StatefulWidget {
 
 class _TripListState extends State<TripList> {
   final List<Widget> _tripTiles = [];
-  final GlobalKey _listKey = GlobalKey();
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
 
   @override
   void initState() {
     super.initState();
-    _addTrips();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _addTrips();
+    });
   }
 
   void _addTrips() {
@@ -29,7 +31,11 @@ class _TripListState extends State<TripList> {
     ];
 
     for (var trip in _trips) {
-      _tripTiles.add(_buildTile(trip));
+      Future.delayed(const Duration(milliseconds: 300)).then((_){
+        _tripTiles.add(_buildTile(trip));
+        _listKey.currentState?.insertItem(_tripTiles.length - 1);
+      });
+
     }
   }
 
@@ -66,13 +72,17 @@ class _TripListState extends State<TripList> {
     );
   }
 
+  final Tween<Offset> _offset =
+      Tween(begin: const Offset(1, 0), end: const Offset(0, 0));
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return AnimatedList(
         key: _listKey,
-        itemCount: _tripTiles.length,
-        itemBuilder: (context, index) {
-          return _tripTiles[index];
+        initialItemCount: _tripTiles.length,
+        itemBuilder: (context, index, animation) {
+          return SlideTransition(
+              position: animation.drive(_offset), child: _tripTiles[index]);
         });
   }
 }
