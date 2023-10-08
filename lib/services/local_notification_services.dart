0eccaff9c5598b2fd6cdaaa0509b087aa52b4d78
theaayushb <aayush.bhattarai@nexdigitalstudio.com>
2023-10-08 13:13:48 +0545

    upgraded packages and removed unused code

--- lib/services/local_notification_services.dart
+++ lib/services/local_notification_services.dart
@@ -13,12 +13,6 @@ class LocalNotificationService {
         InitializationSettings(
             android: AndroidInitializationSettings("@mipmap/ic_launcher"));
 
-    // _notificationsPlugin.initialize(initializationSettings,
-    //     onSelectNotification: (String? route) async {
-    //   if (route != null) {
-    //     Navigator.pushNamed(context, route);
-    //   }
-    // });
     _notificationsPlugin.initialize(
       initializationSettings,
       onDidReceiveNotificationResponse: (details) {
